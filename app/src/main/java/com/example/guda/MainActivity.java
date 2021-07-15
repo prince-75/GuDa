f749ce6bd0fd25ae595a280c3c1820d2550ca85a
prince-75 <pricne18326130885@163.com>
2021-07-15 18:40:40 +0800

    增加了最上方标题栏自定义的控件TitleLayoutb，并在四个页面中引用它

--- app/src/main/java/com/example/guda/MainActivity.java
+++ app/src/main/java/com/example/guda/MainActivity.java
@@ -17,6 +17,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.io.File;
