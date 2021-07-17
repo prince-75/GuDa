1349bdeecf2a4fbe3972a08acc1655972c29135f
LUOXHEI <1046506296@qq.com>
2021-07-17 17:47:41 +0800

    modified: 增加 WiFi 开启按钮和相应功能

--- app/src/main/java/com/example/guda/SecondActivity.java
+++ app/src/main/java/com/example/guda/SecondActivity.java
@@ -1,30 +1,33 @@
 package com.example.guda;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondActivity extends AppCompatActivity {
+import com.example.guda.Network.PopupMenuDialog;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SecondActivity extends AppCompatActivity implements Animator.AnimatorListener {
+    @BindView(R.id.botton_intent)
+    FloatingActionButton botton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        //隐藏系统自带标题栏
-        getSupportActionBar().hide();
-        Button button1 = (Button)findViewById(R.id.button_intent);
-        button1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("https://cn.bing.com/"));
-                startActivity(intent);
-            }
-        });
+        //绑定
+        ButterKnife.bind(this);
+
         //导航栏按钮
         Button button2 = (Button)findViewById(R.id.button1);
         Button button3 = (Button)findViewById(R.id.button3);
@@ -50,5 +53,35 @@ public class SecondActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @OnClick(R.id.botton_intent)
+    public void onClick(View view) {
+            ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(botton, "translationY", 0, botton
+                    .getHeight() * 2).setDuration(200L);
+            objectAnimator.setInterpolator(new AccelerateInterpolator());
+            objectAnimator.addListener(this);
+            objectAnimator.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+        new PopupMenuDialog(this).builder().setCancelable(false)
+                .setCanceledOnTouchOutside(true).show();
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
     }
 }
