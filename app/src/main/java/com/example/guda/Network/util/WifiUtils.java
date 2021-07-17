f1465800be5249c2f3497cd648cd497799ae4d19
LUOXHEI <1046506296@qq.com>
2021-07-17 17:21:35 +0800

    add: 网络模块的工具类的 Wifi 工具类

---
+++ app/src/main/java/com/example/guda/Network/util/WifiUtils.java
@@ -0,0 +1,38 @@
+package com.example.guda.Network.util;
+
+import android.util.Log;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+
+public class WifiUtils {
+    private static final String TAG = "WifiUtils";
+
+    public static String getDeviceIpAddress() {
+        String deviceIpAddress = "###.###.###.###";
+
+        try {
+            for (Enumeration<NetworkInterface> enumeration =
+                 NetworkInterface.getNetworkInterfaces(); enumeration.hasMoreElements(); ) {
+                NetworkInterface networkInterface = enumeration.nextElement();
+
+                for (Enumeration<InetAddress> enumerationIpAddr =
+                     networkInterface.getInetAddresses(); enumerationIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumerationIpAddr.nextElement();
+
+                    if (!inetAddress.isLoopbackAddress() && inetAddress.getAddress().length == 4) {
+                        deviceIpAddress = inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e(TAG, "SocketException:" + e.getMessage());
+        }
+
+        return deviceIpAddress;
+    }
+}
+
