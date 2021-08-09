c27366c5d348ba5cbd674fd4fe92ce054cb6d691
LUOXHEI <1046506296@qq.com>
2021-08-09 11:28:36 +0800

    modified: 细化了 APP 的储存位置、视频储存位置和数据储存位置

--- app/src/main/java/com/example/guda/Network/Constants.java
+++ app/src/main/java/com/example/guda/Network/Constants.java
@@ -6,10 +6,16 @@ import java.io.File;
 
 public class Constants {
     public static final int HTTP_PORT = 12345;
-    public static final String DIR_IN_SDCARD = "FileTransfer";
+    public static final String DIR_IN_SDCARD = "Guda";
+    public static final String Video_DIR_IN_SDCARD = "Guda/video";
+    public static final String Data_DIR_IN_SDCARD = "Guda/data";
     public static final int MSG_DIALOG_DISMISS = 0;
     public static final File DIR = new File(Environment.getExternalStorageDirectory() + File
             .separator + Constants.DIR_IN_SDCARD);
+    public static final File VideoDIR = new File(Environment.getExternalStorageDirectory() +
+            File.separator + Constants.Video_DIR_IN_SDCARD);
+    public static final File DataDIR = new File(Environment.getExternalStorageDirectory() +
+            File.separator + Constants.Data_DIR_IN_SDCARD);
 
     public static final class RxBusEventType {
         public static final String POPUP_MENU_DIALOG_SHOW_DISMISS = "POPUP MENU DIALOG SHOW " +
